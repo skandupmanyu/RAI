224c58162a11f4e69c25e12e60435c69eac2cfbf
weulas <109556479+weulas@users.noreply.github.com>
2022-07-19 07:21:44 -0700

    Add files via upload

---
+++ src/constants/filenames.py
@@ -0,0 +1,11 @@
+"""
+Filenames constants
+"""
+
+INPUT_DATASET = 'usa_IPUMS_2022.csv.gz'
+MODEL = 'model'
+METRICS = 'metrics'
+CROSS_TAB_METRICS = 'cross_tab_accuracy'
+DATASET = 'dataset.csv'
+BIAS_RANK_ACTUAL = 'bias_rank_actual'
+BIAS_RANK_PROXY = 'bias_rank_proxy'
